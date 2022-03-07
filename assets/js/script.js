333bac2fff9e216518b76e10a8b54ff38c176012
aniyahmb99 <aniyah_mb@yahoo.com>
2022-03-06 22:29:34 -0500

    testing functions to show questions/answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,57 @@
-let questions = ["Arrays in Javascript can be used to store ____________.",
-    "String values must be enclosed within blank ____________ when being assigned to variables.",
-    "Avery useful tool used during development and debugging for printing content to the debugger is:",
-    "The condition in an if / else statement is enclosed within _________________.", " Commonly used date types do NOT include:"];
+let questions = [
+    {
+        question: "Arrays in Javascript can be used to store ____________.",
+        answers: {
+            1: "numbers and strings",
+            2: "other arrays",
+            3: "booleans",
+            4: "all of the above"
+        },
+        correctAnswer: "4"
+    },
+    {
+        question: "String values must be enclosed within blank ____________ when being assigned to variables.",
+        answers: {
+            1: "commas",
+            2: "curly brackets",
+            3: "quotes",
+            4: "parenthesis"
+        },
+        correctAnswer: "2"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: {
+            1: "JavaScript",
+            2: "terminal/bash",
+            3: "for loops",
+            4: "console.log"
+        },
+        correctAnswer: "4"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within _________________.",
+        answers: {
+            1: "quotes",
+            2: "curly brackets",
+            3: "parenthesis",
+            4: "square brackets"
+        },
+        correctAnswer: "3"
+    },
+    {
+        question: "Commonly used date types do NOT include:",
+        answers: {
+            1: "strings",
+            2: "booleans",
+            3: "alerts",
+            4: "numbers"
+        }, correctAnswer: "3"
+
+
+    }
+];
+
 
 let startBtn = document.querySelector("#startBtnId");
 let time = document.querySelector("h3");
@@ -13,6 +63,8 @@ let timeSecond = 120;
 
 
 
+
+// timer countdown 
 function handleTime() {
     let countDown = setInterval(() => {
         timeSecond--;
@@ -25,16 +77,19 @@ function handleTime() {
 }
 
 
-
 function startQuiz() {
     let message = "Begin!";
     alert(message);
-    document.getElementById("paragraphContainer").style.visibility = "hidden";
-    document.getElementById("paragraphContainer").innerHTML = questions;
 
+    // hides both "Coding Quiz Challenge" and paragraph on button click
+    document.getElementById("paragraphContainer").style.visibility = "hidden";
+    document.getElementById("heading1").style.visibility = "hidden";
+    document.getElementById("startBtnId").style.visibility = "hidden";
+    // sets timer to be started on button click
     handleTime();
 
 }
+
 
 
 
